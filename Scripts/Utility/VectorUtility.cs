a83aa816ada984df547f2bba6df2c84541564e30
SpudTatterson <tchcjhui123@gmail.com>
2023-10-09 19:40:30 +0300

    Added New Round Function

--- Scripts/Utility/VectorUtility.cs
+++ Scripts/Utility/VectorUtility.cs
@@ -14,4 +14,24 @@ public class VectorUtility : MonoBehaviour
     {
         return (to - from).normalized;
     }
+    static public Vector3 Round(Vector3 vector3, int decimalPlaces)
+    {
+        float multiplier = 1;
+        for (int i = 0; i < decimalPlaces; i++)
+        {
+            multiplier *= 10f;
+        }
+        return new Vector3(
+            Mathf.Round(vector3.x * multiplier) / multiplier,
+            Mathf.Round(vector3.y * multiplier) / multiplier,
+            Mathf.Round(vector3.z * multiplier) / multiplier);
+    }
+    static public Vector3 Round(Vector3 vector3)
+    {
+        return new Vector3(
+            Mathf.Round(vector3.x),
+            Mathf.Round(vector3.y),
+            Mathf.Round(vector3.z));
+    }
 }
+
